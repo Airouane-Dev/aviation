ee32350219359e5223c0266fb95ce6d3c9ebd1a5
Airouane <erwan.cadobts@gmail.com>
2021-06-30 08:40:29 +0200

    feat: add seeds.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :flights
-  get 'home/index'
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'home/index'
 
   root 'home#index'
 
